a0248062a108baef5ab3c0abcdaffb2b4bd56744
k_mitsuura <mk1990503@gmail.com>
2020-05-11 01:34:23 +0900

    Day4:update

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from models.models import OnegaiContent
+from models.database import db_session
+from datetime import datetime
 
 # Flaskオブジェクトの生成
 app = Flask(__name__)
@@ -18,6 +20,25 @@ def post():
     name = request.form["name"]
     all_onegai = OnegaiContent.query.all()
     return render_template("index.html", name=name, all_onegai=all_onegai)
+
+
+@app.route("/add", methods=["post"])
+def add():
+    title = request.form["title"]
+    body = request.form["body"]
+    content = OnegaiContent(title, body, datetime.now())
+    db_session.add(content)
+    db_session.commit()
+    return index()
+
+
+@app.route("/update", methods=["post"])
+def update():
+    content = OnegaiContent.query.filter_by(id=request.form["update"]).first()
+    content.title = request.form["title"]
+    content.body = request.form["body"]
+    db_session.commit()
+    return index()
 
 
 if __name__ == "__main__":
