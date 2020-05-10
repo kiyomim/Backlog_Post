179b9f92a0b06cc35060088998fb22cd8f2078a6
k_mitsuura <mk1990503@gmail.com>
2020-05-11 01:13:02 +0900

    finish Day4

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from models.models import OnegaiContent
 
 # Flaskオブジェクトの生成
 app = Flask(__name__)
@@ -8,14 +9,16 @@ app = Flask(__name__)
 @app.route("/index")
 def index():
     name = request.args.get("name")
-    iro = ["red", "blue", "yellow", "green"]
-    return render_template("index.html", name=name, iro=iro)
+    all_onegai = OnegaiContent.query.all()
+    return render_template("index.html", name=name, all_onegai=all_onegai)
+
 
 @app.route("/index", methods=["post"])
 def post():
     name = request.form["name"]
-    iro = ["red", "blue", "yellow", "green"]
-    return render_template("index.html", name=name, iro=iro)
+    all_onegai = OnegaiContent.query.all()
+    return render_template("index.html", name=name, all_onegai=all_onegai)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
